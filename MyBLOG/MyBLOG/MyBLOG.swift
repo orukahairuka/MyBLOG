4b6e32662cfcba994937fdabb8d7dd004785d505
irukahaoruka <erika03301109@gmail.com>
2024-08-16 00:36:44 +0900

    ファイルの構造

---
+++ MyBLOG/MyBLOG/MyBLOG.swift
@@ -0,0 +1,17 @@
+//
+//  MyBLOGApp.swift
+//  MyBLOG
+//
+//  Created by 櫻井絵理香 on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct MyBLOGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
