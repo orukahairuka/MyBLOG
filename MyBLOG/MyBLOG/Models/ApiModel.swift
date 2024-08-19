2cd02dff2d154c1a6fa4fdd2fcfc63653eb0054a
irukahaoruka <erika03301109@gmail.com>
2024-08-19 14:37:15 +0900

    大元はできた

--- MyBLOG/MyBLOG/Models/ApiModel.swift
+++ MyBLOG/MyBLOG/Models/ApiModel.swift
@@ -1,100 +1,203 @@
-
-// ApiModel.swift
-
-import Foundation
-
-struct NotionPage: Identifiable, Codable {
-    let id: String
-    let properties: [String: PageProperty]
-}
-
-struct PageProperty: Codable {
-    let title: [TitleElement]?
-    let richText: [RichTextElement]?
-    let number: Int?
-    let select: SelectOption?
-    // 他のプロパティタイプも必要に応じて追加
-
-    enum CodingKeys: String, CodingKey {
-        case title
-        case richText = "rich_text"
-        case number
-        case select
-    }
-}
-
-struct TitleElement: Codable {
-    let type: String
-    let text: TextContent
-    let annotations: Annotations
-    let plainText: String
-    let href: String?
-
-    enum CodingKeys: String, CodingKey {
-        case type, text, annotations
-        case plainText = "plain_text"
-        case href
-    }
-}
-
-struct RichTextElement: Codable {
-    let type: String
-    let text: TextContent
-    let annotations: Annotations
-    let plainText: String
-    let href: String?
-
-    enum CodingKeys: String, CodingKey {
-        case type, text, annotations
-        case plainText = "plain_text"
-        case href
-    }
-}
-
-struct TextContent: Codable {
-    let content: String
-    let link: Link?
-}
-
-struct Link: Codable {
-    let url: String
-}
-
-struct Annotations: Codable {
-    let bold: Bool
-    let italic: Bool
-    let strikethrough: Bool
-    let underline: Bool
-    let code: Bool
-    let color: String
-}
-
-struct SelectOption: Codable {
-    let id: String
-    let name: String
-    let color: String
-}
-
-struct Block: Identifiable, Codable {
-    let id: String
-    let type: String
-    let paragraph: ParagraphBlock?
-    // 他のブロックタイプも必要に応じて追加
-}
-
-struct ParagraphBlock: Codable {
-    let richText: [RichTextElement]
-
-    enum CodingKeys: String, CodingKey {
-        case richText = "rich_text"
-    }
-}
-
-// API レスポンス用の構造体
-struct NotionResponse: Codable {
-    let results: [NotionPage]
-}
-
-struct BlockResponse: Codable {
-    let results: [Block]
-}
+//import Foundation
+//
+//struct NotionPage: Identifiable, Codable {
+//    let id: String
+//    let properties: [String: PageProperty]
+//}
+//
+//struct PageProperty: Codable {
+//    let type: String
+//    let id: String?
+//    let value: PropertyValue
+//
+//    enum CodingKeys: String, CodingKey {
+//        case type, id
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        type = try container.decode(String.self, forKey: .type)
+//        id = try container.decodeIfPresent(String.self, forKey: .id)
+//
+//        switch type {
+//        case "title":
+//            value = .title(try container.decode([TitleElement].self, forKey: .init(stringValue: type)!))
+//        case "rich_text":
+//            value = .richText(try container.decode([RichTextElement].self, forKey: .init(stringValue: type)!))
+//        case "number":
+//            value = .number(try container.decode(Double.self, forKey: .init(stringValue: type)!))
+//        case "select":
+//            value = .select(try container.decode(SelectOption.self, forKey: .init(stringValue: type)!))
+//        case "multi_select":
+//            value = .multiSelect(try container.decode([SelectOption].self, forKey: .init(stringValue: type)!))
+//        case "date":
+//            value = .date(try container.decode(DateValue.self, forKey: .init(stringValue: type)!))
+//        case "people":
+//            value = .people(try container.decode([User].self, forKey: .init(stringValue: type)!))
+//        case "files":
+//            value = .files(try container.decode([FileObject].self, forKey: .init(stringValue: type)!))
+//        case "checkbox":
+//            value = .checkbox(try container.decode(Bool.self, forKey: .init(stringValue: type)!))
+//        case "url":
+//            value = .url(try container.decode(String.self, forKey: .init(stringValue: type)!))
+//        case "email":
+//            value = .email(try container.decode(String.self, forKey: .init(stringValue: type)!))
+//        case "phone_number":
+//            value = .phoneNumber(try container.decode(String.self, forKey: .init(stringValue: type)!))
+//        case "formula":
+//            value = .formula(try container.decode(FormulaResult.self, forKey: .init(stringValue: type)!))
+//        case "relation":
+//            value = .relation(try container.decode([RelationItem].self, forKey: .init(stringValue: type)!))
+//        case "rollup":
+//            value = .rollup(try container.decode(RollupResult.self, forKey: .init(stringValue: type)!))
+//        case "created_time":
+//            value = .createdTime(try container.decode(String.self, forKey: .init(stringValue: type)!))
+//        case "created_by":
+//            value = .createdBy(try container.decode(User.self, forKey: .init(stringValue: type)!))
+//        case "last_edited_time":
+//            value = .lastEditedTime(try container.decode(String.self, forKey: .init(stringValue: type)!))
+//        case "last_edited_by":
+//            value = .lastEditedBy(try container.decode(User.self, forKey: .init(stringValue: type)!))
+//        default:
+//            throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unknown property type: \(type)")
+//        }
+//    }
+//}
+//
+//enum PropertyValue {
+//    case title([TitleElement])
+//    case richText([RichTextElement])
+//    case number(Double)
+//    case select(SelectOption)
+//    case multiSelect([SelectOption])
+//    case date(DateValue)
+//    case people([User])
+//    case files([FileObject])
+//    case checkbox(Bool)
+//    case url(String)
+//    case email(String)
+//    case phoneNumber(String)
+//    case formula(FormulaResult)
+//    case relation([RelationItem])
+//    case rollup(RollupResult)
+//    case createdTime(String)
+//    case createdBy(User)
+//    case lastEditedTime(String)
+//    case lastEditedBy(User)
+//}
+//
+//struct TitleElement: Codable {
+//    let type: String
+//    let text: TextContent
+//    let annotations: Annotations
+//    let plainText: String
+//    let href: String?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case type, text, annotations
+//        case plainText = "plain_text"
+//        case href
+//    }
+//}
+//
+//struct RichTextElement: Codable {
+//    let type: String
+//    let text: TextContent
+//    let annotations: Annotations
+//    let plainText: String
+//    let href: String?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case type, text, annotations
+//        case plainText = "plain_text"
+//        case href
+//    }
+//}
+//
+//struct TextContent: Codable {
+//    let content: String
+//    let link: Link?
+//}
+//
+//struct Link: Codable {
+//    let url: String
+//}
+//
+//struct Annotations: Codable {
+//    let bold: Bool
+//    let italic: Bool
+//    let strikethrough: Bool
+//    let underline: Bool
+//    let code: Bool
+//    let color: String
+//}
+//
+//struct SelectOption: Codable {
+//    let id: String
+//    let name: String
+//    let color: String
+//}
+//
+//struct DateValue: Codable {
+//    let start: String
+//    let end: String?
+//}
+//
+//struct User: Codable {
+//    let id: String
+//    let name: String?
+//    let avatarUrl: String?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case id, name
+//        case avatarUrl = "avatar_url"
+//    }
+//}
+//
+//struct FileObject: Codable {
+//    let name: String
+//    let url: String?
+//}
+//
+//struct FormulaResult: Codable {
+//    let type: String
+//    let number: Double?
+//    let string: String?
+//    let boolean: Bool?
+//    let date: DateValue?
+//}
+//
+//struct RelationItem: Codable {
+//    let id: String
+//}
+//
+//struct RollupResult: Codable {
+//    let type: String
+//    let number: Double?
+//    let date: DateValue?
+//    let array: [PageProperty]?
+//}
+//
+//struct Block: Identifiable, Codable {
+//    let id: String
+//    let type: String
+//    let paragraph: ParagraphBlock?
+//    // 他のブロックタイプも必要に応じて追加
+//}
+//
+//struct ParagraphBlock: Codable {
+//    let richText: [RichTextElement]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case richText = "rich_text"
+//    }
+//}
+//
+//// API レスポンス用の構造体
+//struct NotionResponse: Codable {
+//    let results: [NotionPage]
+//}
+//
+//struct BlockResponse: Codable {
+//    let results: [Block]
+//}
--- MyBLOG/MyBLOG/Views/ContentView.swift
+++ MyBLOG/MyBLOG/Views/ContentView.swift
@@ -6,41 +6,185 @@ import Alamofire
 
 
 
+
+
+enum PropertyValue {
+    case title([TitleElement])
+    case richText([RichTextElement])
+    // Add other cases as needed
+}
+
+struct TitleElement: Codable {
+    let plainText: String
+
+    enum CodingKeys: String, CodingKey {
+        case plainText = "plain_text"
+    }
+}
+
+struct RichTextElement: Codable {
+    let plainText: String
+
+    enum CodingKeys: String, CodingKey {
+        case plainText = "plain_text"
+    }
+}
+
+struct Block: Identifiable, Codable {
+    let id: String
+    let type: String
+    let paragraph: ParagraphBlock?
+}
+
+struct ParagraphBlock: Codable {
+    let richText: [RichTextElement]
+
+    enum CodingKeys: String, CodingKey {
+        case richText = "rich_text"
+    }
+}
+
+struct NotionResponse: Codable {
+    let object: String
+    let results: [NotionPage]
+    let nextCursor: String?
+    let hasMore: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case object, results
+        case nextCursor = "next_cursor"
+        case hasMore = "has_more"
+    }
+}
+
+struct BlockResponse: Codable {
+    let results: [Block]
+}
+
+struct NotionPage: Identifiable, Codable {
+    let id: String
+    let properties: [String: PageProperty]
+
+    var title: String {
+        if let titleProperty = properties["名前"],
+           let titleContent = titleProperty.title,
+           let firstTitle = titleContent.first {
+            return firstTitle.plainText
+        }
+        return "Untitled"
+    }
+
+    var tags: [String] {
+        if let tagProperty = properties["タグ"],
+           let multiSelect = tagProperty.multiSelect {
+            return multiSelect.map { $0.name }
+        }
+        return []
+    }
+}
+
+struct PageProperty: Codable {
+    let id: String?
+    let type: String
+    let multiSelect: [MultiSelectOption]?
+    let title: [TextContent]?
+    let richText: [TextContent]?
+    // 他のプロパティタイプも必要に応じて追加
+
+    enum CodingKeys: String, CodingKey {
+        case id, type
+        case multiSelect = "multi_select"
+        case title
+        case richText = "rich_text"
+    }
+}
+
+struct MultiSelectOption: Codable {
+    let id: String
+    let name: String
+    let color: String
+}
+
+struct TextContent: Codable {
+    let plainText: String
+    let href: String?
+
+    enum CodingKeys: String, CodingKey {
+        case plainText = "plain_text"
+        case href
+    }
+}
 // MARK: - NotionApiClient
 
 class NotionApiClient {
     private let baseURL = "https://api.notion.com/v1"
-    private let apiKey: String
     private let headers: HTTPHeaders
-    
+    private let session: Session
+
     init(apiKey: String) {
-        self.apiKey = apiKey
         self.headers = [
-            "Accept": "application/json",
+            "Authorization": "Bearer \(apiKey)",
             "Notion-Version": "2022-06-28",
-            "Authorization": "Bearer \(apiKey)"
+            "Content-Type": "application/json"
         ]
+
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 30
+        configuration.connectionProxyDictionary = [:] // プロキシ設定をバイパス
+        self.session = Session(configuration: configuration)
     }
-    
+
     func queryDatabase(databaseId: String) -> AnyPublisher<[NotionPage], Error> {
-        let url = "\(baseURL)/databases/\(databaseId)/query"
-        return AF.request(url, method: .post, headers: headers)
+        let encodedDatabaseId = databaseId.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? databaseId
+        let url = "\(baseURL)/databases/\(encodedDatabaseId)/query"
+
+        return session.request(url, method: .post, headers: headers)
             .validate()
             .publishDecodable(type: NotionResponse.self)
-            .value()
-            .map { $0.results }
-            .mapError { $0 as Error }
+            .tryMap { response -> [NotionPage] in
+                if let data = response.data {
+                    print("Raw API Response:")
+                    if let jsonString = String(data: data, encoding: .utf8) {
+                        print(jsonString)
+                    }
+                }
+
+                switch response.result {
+                case .success(let notionResponse):
+                    return notionResponse.results
+                case .failure(let error):
+                    throw error
+                }
+            }
+            .mapError { error in
+                print("Error in queryDatabase: \(error)")
+                if let decodingError = error as? DecodingError {
+                    print("Decoding error details: \(decodingError)")
+                }
+                return error
+            }
             .eraseToAnyPublisher()
     }
-    
+
     func getPageBlocks(pageId: String) -> AnyPublisher<[Block], Error> {
-        let url = "\(baseURL)/blocks/\(pageId)/children"
-        return AF.request(url, method: .get, headers: headers)
+        let encodedPageId = pageId.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? pageId
+        let url = "\(baseURL)/blocks/\(encodedPageId)/children"
+
+        return session.request(url, method: .get, headers: headers)
             .validate()
             .publishDecodable(type: BlockResponse.self)
-            .value()
-            .map { $0.results }
-            .mapError { $0 as Error }
+            .tryMap { response -> [Block] in
+                switch response.result {
+                case .success(let blockResponse):
+                    return blockResponse.results
+                case .failure(let error):
+                    throw error
+                }
+            }
+            .mapError { error in
+                print("Error in getPageBlocks: \(error)")
+                return error
+            }
             .eraseToAnyPublisher()
     }
 }
@@ -50,55 +194,77 @@ class NotionApiClient {
 class DatabaseViewModel: ObservableObject {
     @Published var pages: [NotionPage] = []
     @Published var isLoading = false
-    @Published var error: IdentifiableError?
-    
-    let apiClient: NotionApiClient  // privateを削除
+    @Published var error: Error?
+
+    let apiClient: NotionApiClient
     private var cancellables = Set<AnyCancellable>()
-    
+
     init(apiClient: NotionApiClient) {
         self.apiClient = apiClient
     }
-    
+
     func fetchPages(databaseId: String) {
         isLoading = true
         error = nil
-        
+
         apiClient.queryDatabase(databaseId: databaseId)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] completion in
                 self?.isLoading = false
                 if case .failure(let error) = completion {
-                    self?.error = IdentifiableError(error: error)
+                    self?.error = error
                 }
             } receiveValue: { [weak self] pages in
                 self?.pages = pages
             }
             .store(in: &cancellables)
     }
+
+    func getPageTitle(_ page: NotionPage) -> String {
+        if let titleProperty = page.properties["名前"],
+           let titleContent = titleProperty.title,
+           let firstTitle = titleContent.first {
+            return firstTitle.plainText
+        }
+        print("Failed to get title for page: \(page.id)")
+        print("Available properties: \(page.properties.keys)")
+        return "Untitled"
+    }
+
+    func getPageTags(_ page: NotionPage) -> [String] {
+        if let tagProperty = page.properties["タグ"],
+           let multiSelect = tagProperty.multiSelect {
+            return multiSelect.map { $0.name }
+        }
+        print("Failed to get tags for page: \(page.id)")
+        print("Available properties: \(page.properties.keys)")
+        return []
+    }
 }
 
 class PageViewModel: ObservableObject {
     @Published var blocks: [Block] = []
     @Published var isLoading = false
-    @Published var error: IdentifiableError?
-    
-    let apiClient: NotionApiClient  // privateを削除
-    private var cancellables = Set<AnyCancellable>()
-    
+    @Published var error: Error?
+
+    let apiClient: NotionApiClient
+    var cancellables = Set<AnyCancellable>()
+
     init(apiClient: NotionApiClient) {
         self.apiClient = apiClient
     }
-    
+
     func fetchBlocks(pageId: String) {
         isLoading = true
         error = nil
-        
+
         apiClient.getPageBlocks(pageId: pageId)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] completion in
                 self?.isLoading = false
                 if case .failure(let error) = completion {
-                    self?.error = IdentifiableError(error: error)
+                    self?.error = error
+                    print("Error in getPageBlocks: \(error)")
                 }
             } receiveValue: { [weak self] blocks in
                 self?.blocks = blocks
@@ -106,6 +272,8 @@ class PageViewModel: ObservableObject {
             .store(in: &cancellables)
     }
 }
+
+
 
 // MARK: - IdentifiableError
 
@@ -119,12 +287,24 @@ struct IdentifiableError: Identifiable {
 struct DatabaseListView: View {
     @ObservedObject var viewModel: DatabaseViewModel
     let databaseId: String
-    
+
     var body: some View {
         NavigationView {
             List(viewModel.pages) { page in
                 NavigationLink(destination: PageDetailView(viewModel: PageViewModel(apiClient: viewModel.apiClient), pageId: page.id)) {
-                    Text(page.properties["Name"]?.title?.first?.plainText ?? "Untitled")
+                    VStack(alignment: .leading) {
+                        Text(page.title)
+                            .font(.headline)
+                        HStack {
+                            ForEach(page.tags, id: \.self) { tag in
+                                Text(tag)
+                                    .font(.caption)
+                                    .padding(4)
+                                    .background(Color.blue.opacity(0.1))
+                                    .cornerRadius(4)
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Notion Database")
@@ -136,15 +316,12 @@ struct DatabaseListView: View {
                     ProgressView()
                 }
             })
-            .alert(item: $viewModel.error) { error in
-                Alert(title: Text("Error"), message: Text(error.error.localizedDescription))
-            }
         }
     }
 }
 
 struct PageDetailView: View {
-    @ObservedObject var viewModel: PageViewModel
+    @StateObject var viewModel: PageViewModel
     let pageId: String
 
     var body: some View {
@@ -158,30 +335,40 @@ struct PageDetailView: View {
                 .padding()
             }
             .navigationTitle("Page Detail")
-            .onAppear {
-                viewModel.fetchBlocks(pageId: pageId)
-            }
-            .overlay(Group {
-                if viewModel.isLoading {
-                    ProgressView()
-                }
-            })
-            .alert(item: $viewModel.error) { error in
-                Alert(title: Text("Error"), message: Text(error.error.localizedDescription))
-            }
+        }
+        .onAppear {
+            viewModel.fetchBlocks(pageId: pageId)
+        }
+        .onDisappear {
+            // キャンセル処理を追加
+            viewModel.cancellables.removeAll()
         }
     }
+}
 
-    // MARK: - App
+// MARK: - App
 
-    @main
-    struct NotionViewerApp: App {
-        let apiClient = NotionApiClient(apiKey: "secret_5izAgKnmuRtYqCtZ79J3PobSlleh7WGoFdOKuFdOBge")
 
-        var body: some Scene {
-            WindowGroup {
-                DatabaseListView(viewModel: DatabaseViewModel(apiClient: apiClient), databaseId: "c5a35870426c49f0b7669991b1c92fa6")
-            }
+
+@main
+struct NotionViewerApp: App {
+    let apiClient: NotionApiClient
+
+    init() {
+        // APIキーを安全な方法で管理する
+#if DEBUG
+        let apiKey = "secret_5izAgKnmuRtYqCtZ79J3PobSlleh7WGoFdOKuFdOBge" // 開発時はハードコードされた値を使用
+#else
+        // 本番環境では、より安全な方法（例：キーチェーン）を使用
+        let apiKey = KeychainService.retrieveAPIKey() ?? ""
+#endif
+
+        self.apiClient = NotionApiClient(apiKey: apiKey)
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            DatabaseListView(viewModel: DatabaseViewModel(apiClient: apiClient), databaseId: "c5a35870426c49f0b7669991b1c92fa6")
         }
     }
 }
